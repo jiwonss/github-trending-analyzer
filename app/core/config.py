5e92e3c14bff293474ffb9d88ddcbd8aabadb58c
jiownss <sjs3326@naver.com>
2024-12-23 22:14:39 +0900

    feat(core): 환경 변수 로드 설정 클래스 추가

---
+++ app/core/config.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    github_api_base_url: str
+    github_token: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
